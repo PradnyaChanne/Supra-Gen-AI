910444eb17d6f82655c4a3ce3d78d20dc048167e
Pradnya Naresh Channe <pradnya.channe@tce.co.in>
2024-10-22 14:18:51 +0530

    first commit

---
+++ src/components/Main/Main.js
@@ -0,0 +1,81 @@
+import React, { useContext } from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+import { Context } from '../../context/context'
+const Main = () => {
+
+    const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context)
+
+    return (
+        <div className='main'>
+            <div className='nav'>
+                <p>Supra</p>
+                <img src={assets.bear} alt='' />
+            </div>
+            <div className='main-container'>
+
+                {!showResult
+                    ? <>
+                        <div className='greet'>
+                            <p><span>Hello, Dev</span></p>
+                            <p>How Can I help you Today?</p>
+                        </div>
+                        <div className='cards'>
+                            <div className='card'>
+                                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                                <img src={assets.compass_icon} alt='' />
+                            </div>
+                            <div className='card'>
+                                <p>Test my knowledge on ancient civilizations</p>
+                                <img src={assets.bulb_icon} alt='' />
+                            </div>
+                            <div className='card'>
+                                <p>Write a message that goes with a kitten gif for a friend on a rough day</p>
+                                <img src={assets.message_icon} alt='' />
+                            </div>
+                            <div className='card'>
+                                <p>Design a programming game</p>
+                                <img src={assets.code_icon} alt='' />
+                            </div>
+                        </div>
+                    </>
+                    : <div className='result'>
+                        <div className='result-title'>
+                            <img src={assets.bear} alt='' />
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className='result-data'>
+                            <img src={assets.gemini_icon} alt='' />
+                            {loading?
+                                <div className='loader'>
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                </div>:
+                                <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                        }
+                            
+                        </div>
+
+                    </div>
+                }
+
+                <div className='main-bottom'>
+                    <div className='search-box'>
+                        <input onChange={(e) => setInput(e.target.value)} value={input} type='text' placeholder='Enter a prompt here' />
+                        <div>
+                            <img src={assets.gallery_icon} alt='' />
+                            <img src={assets.mic_icon} alt='' />
+                            {input ?<img onClick={() => onSent()} src={assets.send_icon} alt='' />:null}
+                        </div>
+                    </div>
+                    <p className='bottom-info'>
+                        Supra can make mistakes. Check important info.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
